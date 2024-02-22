7128941809c7a0d90dd6e1a08a0132fcff984b40
Musihin <musihinalexey@gmail.com>
2024-02-22 19:26:03 +0300

    Создание галереи

--- sitemycat/mycat/views.py
+++ sitemycat/mycat/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.template.loader import render_to_string
 from  django.shortcuts import render
+import os
+from django.conf import settings
+
+
+
+
+
 # Create your views here.
 
 
@@ -12,11 +19,15 @@ def index(request):
 
 
 def video(request):
-    return render( request,'mycat/video.html')
+    return render(request, 'mycat/video.html')
 
 
 def photo(request):
-    return render(request,'mycat/photo.html')
+    image_dir = r'D:\Python\mycat\sitemycat\mycat\static\mycat\image\photos'
+    images = os.listdir(image_dir)
+    print(images)
+
+    return render(request, 'mycat/photo.html', {'images': images})
 
 
 def page_not_found(request, exception):
